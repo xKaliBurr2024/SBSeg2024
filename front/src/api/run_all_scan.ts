85a27d0f10525914040066f3d7a921c6ddffba17
jva411 <j.victoralves411@gmail.com>
2024-06-18 16:14:53 -0300

    chore: improving the return of RunAllScan function

--- front/src/api/run_all_scan.ts
+++ front/src/api/run_all_scan.ts
@@ -4,13 +4,15 @@ export async function RunAllScan(input: string, protocol: 'http' | 'https') {
     const url = `${protocol}://${input}`
     const host = input.split('/')[0].split(':')[0]
     const ip = (await (await getIP(host)).text()).split(' ').at(-1)!
-    const promises = [
-        getReverseDNS(ip),
-        getSubDNS(host),
-        getWhoIs(ip),
-        getBanner(url),
-        getDirectoryScan(ip),
-        getPorts(ip)
-    ]
-    return promises
+
+    const promises = {
+        reverseDNS: getReverseDNS(ip),
+        subDNS: getSubDNS(host),
+        whoIs: getWhoIs(ip),
+        banner: getBanner(url),
+        directoryScan: getDirectoryScan(ip),
+        ports: getPorts(ip)
+    }
+
+    return { promises, ip }
 }

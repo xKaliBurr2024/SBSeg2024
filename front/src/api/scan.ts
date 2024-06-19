c4dda383f9d6ca3b8bee0acc3fe012360035ab3e
fmcastro2 <felipe.moura@lsbd.ufc.br>
2024-06-18 22:31:09 -0300

    Fixing request

--- front/src/api/scan.ts
+++ front/src/api/scan.ts
@@ -9,8 +9,8 @@ export function getReverseDNS(ip: string) {
 }
 
 export function getSubDNS(host: string) {
-    // return customFetch(`/sub_dns?host=${host}`, {method: 'GET'})
-    return {text: () => "Testando"}
+    return customFetch(`/sub_dns?host=${host}`, {method: 'GET'})
+    // return {text: () => "Testando"}
 }
 
 export function getWhoIs(ip: string) {
@@ -22,8 +22,8 @@ export function getBanner(url: string) {
 }
 
 export function getDirectoryScan(ip: string) {
-    // return customFetch(`/directory_scan?ip=${ip}`, {method: 'GET'})
-    return {text: () => "Testando2"}
+    return customFetch(`/directory_scan?ip=${ip}`, {method: 'GET'})
+    // return {text: () => "Testando2"}
 }
 
 export function getPorts(ip: string) {
